42f811c4358c2b050b8c87ffdfd0ec57947cc924
Niraj Kumar <niranjan888k@gmail.com>
2024-03-21 00:14:57 +0530

    Add files via upload

---
+++ many-to-many-uni/src/main/java/org/jsp/ManytomanyApp/Batch.java
@@ -0,0 +1,73 @@
+package org.jsp.ManytomanyApp;
+
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+public class Batch {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(nullable = false, unique = true)
+	private String batch_code;
+
+	@Column(nullable = false)
+	private String subject, trainner;
+
+	@ManyToMany(cascade = CascadeType.ALL)
+
+	@JoinTable(name = "batch_student", joinColumns = { @JoinColumn(name = "batch_id") }, inverseJoinColumns = {
+			@JoinColumn(name = "student_id") })
+
+	private List<Student> students;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getBatch_code() {
+		return batch_code;
+	}
+
+	public void setBatch_code(String batch_code) {
+		this.batch_code = batch_code;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getTrainner() {
+		return trainner;
+	}
+
+	public void setTrainner(String trainner) {
+		this.trainner = trainner;
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		return "Batch [id=" + id + ", batch_code=" + batch_code + ", subject=" + subject + ", trainner=" + trainner
+				+ "]";
+	}
+
+}
---
+++ many-to-many-uni/src/main/java/org/jsp/ManytomanyApp/SaveStudenAndBatch.java
@@ -0,0 +1,66 @@
+package org.jsp.ManytomanyApp;
+
+import java.util.Arrays;
+
+import javax.persistence.*;
+
+public class SaveStudenAndBatch {
+
+	public static void main(String[] args) {
+
+		Student s1 = new Student();
+		s1.setName("raja");
+		s1.setPhone(1234);
+		s1.setPerc(75.45);
+
+		Student s2 = new Student();
+		s2.setName("niraj");
+		s2.setPhone(123456);
+		s2.setPerc(79.45);
+
+		Student s3 = new Student();
+		s3.setName("rajesh");
+		s3.setPhone(123478);
+		s3.setPerc(85.45);
+
+		Batch b1 = new Batch();
+		b1.setBatch_code("j123");
+		b1.setSubject("java");
+		b1.setTrainner("keshava sir");
+		b1.setStudents(Arrays.asList(s1, s2, s3));
+
+		Batch b2 = new Batch();
+		b2.setBatch_code("pr123");
+		b2.setSubject("programing");
+		b2.setTrainner("Dharshan sir");
+		b2.setStudents(Arrays.asList(s1, s2, s3));
+
+		Batch b3 = new Batch();
+		b3.setBatch_code("sql123");
+		b3.setSubject("SQL");
+		b3.setTrainner("Sweetha");
+		b3.setStudents(Arrays.asList(s1, s3));
+
+		Batch b4 = new Batch();
+		b4.setBatch_code("h123");
+		b4.setSubject("j2ee");
+		b4.setTrainner("Shathish sir");
+		b3.setStudents(Arrays.asList(s2, s3));
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("development");
+		EntityManager manager = factory.createEntityManager();
+		EntityTransaction transaction = manager.getTransaction();
+
+		manager.persist(b1);
+		manager.persist(b2);
+		manager.persist(b3);
+		manager.persist(b4);
+		
+		
+		transaction.begin();
+		transaction.commit();
+		
+
+	}
+
+}
